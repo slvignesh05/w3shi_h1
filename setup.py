59479466cb0a39bf6b6ce7b2429e1baeb097066e
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-08-08 18:47:21 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+print("installed from package")
+
+setup(
+    name='w3shi_h1',
+    version='0.0.1',
+    packages=['w3shi_h1'],
+    description='Demo package for PyPI publishing test',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your@email.com',
+    url='https://github.com/slvignesh05/w3shi_h1',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
